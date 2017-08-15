7cf5d13c37dd50fa485f4666de3b4115df6c54d1
Prashant Sharma <12ps93r@gmail.com>
2017-08-15 19:48:51 +0530

    Inserted tables of 1 to 10 part2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public static void main(String args[]){
 System.out.println("Hello World");
 for (int i=1; i<=10; i++)
 {
+System.out.println("::::Table of"+i+"::::");
 for (int j=1;j<=10;j++)
 System.out.println(i+"*"+j+"="+i*j);
 System.out.println("\n");
