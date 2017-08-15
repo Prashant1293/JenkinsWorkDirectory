227b751567deab3f570292aaf33c419dc7ec6cda
Prashant Sharma <12ps93r@gmail.com>
2017-08-15 19:47:04 +0530

    Inserted tables of 1 to 10

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,12 @@
 public class HelloWorld{
 public static void main(String args[]){
 System.out.println("Hello World");
+for (int i=1; i<=10; i++)
+{
+for (int j=1;j<=10;j++)
+System.out.println(i+"*"+j+"="+i*j);
+System.out.println("\n");
+}
+
 }
 }
